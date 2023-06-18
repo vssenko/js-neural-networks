40e63ad7a578fc2f6ed4e4809161094aa2c5e78a
Vitaly Senko <senko.vitaly@gmail.com>
2023-06-18 22:20:45 +0300

    Add testing serialized network.

---
+++ 01-ffn-perceptron-manual/scripts/test-serialized-mnist.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const serializer = require('../src/network-management/network-serializer')
+const trainer = require('../src/network-management/network-trainer');
+const mnistProvider = require('../src/mnist/train-data-provider');
+
+const filePath = process.argv[2]
+if (!filePath) throw new Error('No file was provided');
+
+if (filePath.startsWith('./')) {
+  filePath = path.join(process.cwd(), filePath)
+}
+
+
+console.log(`Using file "${filePath}"`);
+
+const network = serializer.deserializeFromFile(filePath)
+
+const mnistTestData = mnistProvider.getTestData();
+
+trainer.test(network, mnistTestData);
+
+console.log('Example run on first 10 mnist test images: ');
+
+for (let i = 0; i < 10; i++) {
+  const sample = mnistTestData[i];
+  const result = network.run(sample.input);
+
+  console.log(`${sample.label}:${result}`);
+}
